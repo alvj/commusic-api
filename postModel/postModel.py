309254bdf04d052d7399d3f1f5e58ca83384e27d
Ian Vidriales Fidalgo <ianvidriales@gmail.com>
2023-01-23 19:12:16 +0100

    Add Model Post

---
+++ postModel/postModel.py
@@ -0,0 +1,12 @@
+import datetime
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Post(BaseModel):
+    post_id: int
+    user_id: int
+    price: int
+    title: str
+    upload_date: datetime.date
+    description: str
