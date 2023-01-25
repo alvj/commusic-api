54a070f756c8c6401b8e043651e35ce5126bf445
Ian Vidriales Fidalgo <ianvidriales@gmail.com>
2023-01-25 22:56:52 +0100

    Fix linter warnings for models

--- models/category.py
+++ models/category.py
@@ -1,7 +1,8 @@
-import datetime
+import dataclasses
 from pydantic import BaseModel
 
-
+@dataclasses.dataclass
 class Category(BaseModel):
+    """A dummy docstring."""
     category_name: str
     category_id: int
