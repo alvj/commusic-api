20c6c604045293115343950ec69616a41632c4cd
Álvaro <alvarojperezperez@gmail.com>
2023-02-10 12:35:24 +0100

    Create auth router and move /token endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
 from .database import create_db_and_tables
 from .models import Post, PostCreate, PostRead, PostReadWithPhotos, Token, User, UserRead
 from .dependencies import authenticate_user, create_access_token, get_current_user, get_session
@@ -13,18 +12,6 @@ def on_startup():
 @app.get("/health", status_code=status.HTTP_200_OK)
 def health_check():
     return "OK"
-
-@app.post("/token", response_model=Token)
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    access_token = create_access_token(data={"sub": user.username})
-    return Token(access_token=access_token, token_type="bearer")
 
 @app.get("/users/me", response_model=UserRead)
 async def read_current_user(current_user: User = Depends(get_current_user)):
