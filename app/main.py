738424ea6c3f914bf5b65972fac794b38148626c
Álvaro <alvarojperezperez@gmail.com>
2023-02-10 12:33:41 +0100

    Create posts router and move endpoints from main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from sqlmodel import Session, select
 from .database import create_db_and_tables
 from .models import Post, PostCreate, PostRead, PostReadWithPhotos, Token, User, UserRead
 from .dependencies import authenticate_user, create_access_token, get_current_user, get_session
@@ -14,34 +13,6 @@ def on_startup():
 @app.get("/health", status_code=status.HTTP_200_OK)
 def health_check():
     return "OK"
-
-@app.post("/posts", response_model=PostRead)
-def create_post(post: PostCreate, session: Session = Depends(get_session)):
-    db_post = Post.from_orm(post)
-    session.add(db_post)
-    session.commit()
-    session.refresh(db_post)
-    return db_post
-
-@app.get("/posts", response_model=list[PostRead])
-def read_posts(session: Session = Depends(get_session)) :
-    posts = session.exec(select(Post)).all()
-    return posts
-
-@app.get("/posts/{post_id}", response_model=PostReadWithPhotos)
-def read_post_details(post_id: int, session: Session = Depends(get_session)):
-    post = session.get(Post, post_id)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-    return post
-
-@app.delete("/posts/{post_id}")
-def delete_post(post_id: int, session: Session = Depends(get_session)):
-    post = session.get(Post, post_id)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
-    session.delete(post)
-    session.commit()
 
 @app.post("/token", response_model=Token)
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
---
+++ app/routers/posts.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlmodel import Session, select
+from ..dependencies import get_session
+from ..models import Post, PostRead, PostReadWithPhotos, PostCreate
+
+router = APIRouter(
+    prefix="/posts",
+    tags=["Posts"],
+    responses={404: {"description": "Not found"}}
+)
+
+
+@router.post("/", response_model=PostRead, status_code=status.HTTP_201_CREATED)
+def create_post(post: PostCreate, session: Session = Depends(get_session)):
+    db_post = Post.from_orm(post)
+    session.add(db_post)
+    session.commit()
+    session.refresh(db_post)
+    return db_post
+
+
+@router.get("/", response_model=list[PostRead])
+def read_posts(session: Session = Depends(get_session)):
+    posts = session.exec(select(Post)).all()
+    return posts
+
+
+@router.get("/{post_id}", response_model=PostReadWithPhotos)
+def read_post_details(post_id: int, session: Session = Depends(get_session)):
+    post = session.get(Post, post_id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return post
+
+
+@router.delete("/{post_id}")
+def delete_post(post_id: int, session: Session = Depends(get_session)):
+    post = session.get(Post, post_id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    session.delete(post)
+    session.commit()
