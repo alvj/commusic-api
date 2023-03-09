74175fab3a97af2957af34770afa089515e1fe83
Álvaro Pérez <alvarojperezperez@gmail.com>
2023-03-09 12:22:31 +0100

    Add TypeVar to crud update entity

--- app/crud.py
+++ app/crud.py
@@ -1,7 +1,10 @@
+from typing import TypeVar
 from sqlmodel import SQLModel, Session
 
+Model = TypeVar("Model", bound=SQLModel)
 
-def update_entity(session: Session, current_entity: SQLModel, new_entity: SQLModel) -> SQLModel:
+
+def update_entity(session: Session, current_entity: Model, new_entity: Model) -> Model:
     new_entity_data = new_entity.dict(exclude_unset=True)
     for key, value in new_entity_data.items():
         setattr(current_entity, key, value)
